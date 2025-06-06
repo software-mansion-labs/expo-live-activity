79040c753f020c9ee636b10002bc1cc154cce810
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-06-06 14:36:45 +0200

    Improve timer controls

--- example/App.tsx
+++ example/App.tsx
@@ -1,27 +1,31 @@
 import * as LiveActivity from "expo-live-activity";
-import { Button, StyleSheet, TextInput, View, Text } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Keyboard,
+  Pressable,
+} from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [activityId, setActivityID] = useState<String | null>();
   const [title, onChangeTitle] = useState("");
   const [subtitle, onChangeSubtitle] = useState("");
-  const [minutes, onChangeMinutes] = useState('');
+  const [minutes, setMinutes] = useState(5);
 
   const startActivity = () => {
-    let timerMinutes = +minutes
-    if (timerMinutes) {
-      const state = {
-        title: title,
-        subtitle: subtitle,
-        date: Date.now() + timerMinutes * 60 * 1000,
-      };
-      const id = LiveActivity.startActivity(state);
-      console.log(id);
-      setActivityID(id);
-    } else {
-      console.log("Enter valid time")
-    }
+    Keyboard.dismiss();
+    const state = {
+      title: title,
+      subtitle: subtitle,
+      date: Date.now() + minutes * 60 * 1000,
+    };
+    const id = LiveActivity.startActivity(state);
+    console.log(id);
+    setActivityID(id);
   };
 
   const stopActivity = () => {
@@ -35,7 +39,7 @@ export default function App() {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
       <Text style={styles.label}>Set Live Activity title:</Text>
       <TextInput
         style={styles.input}
@@ -51,18 +55,20 @@ export default function App() {
         value={subtitle}
       />
       <Text style={styles.label}>Set Live Activity timer:</Text>
-      <TextInput
-          style={styles.input}
-          onChangeText={onChangeMinutes}
-          value={minutes}
-          placeholder="Minutes"
-          keyboardType="numeric"
-        />
+      <View style={styles.timerControlsContainer}>
+        <Pressable style={styles.minus} onPress={() => { setMinutes(minutes - 1)}} disabled={minutes <= 0 }>
+          <Text style={styles.timerControlsText}>-</Text>
+        </Pressable>
+        <Text style={styles.timerControlsText}>{minutes}</Text>
+        <Pressable style={styles.plus} onPress={() => { setMinutes(minutes + 1)}} disabled={minutes >= 60}>
+          <Text style={styles.timerControlsText}>+</Text>
+        </Pressable>
+      </View>
       <View style={styles.buttonsContainer}>
         <Button
           title="Start Activity"
           onPress={startActivity}
-          disabled={title === ""}
+          disabled={title === "" || !!activityId }
         />
         <Button
           title="Stop Activity"
@@ -75,16 +81,44 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  timerControlsContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    margin: 10,
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  minus: {
+    marginRight: 50,
+    width: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  plus: {
+    marginLeft: 50,
+    width: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  timerControlsText: {
+    fontSize: 17
+  },
   buttonsContainer: {
-    padding: 20
+    padding: 20,
   },
   label: {
-    width: '90%',
+    width: "90%",
     fontSize: 17,
   },
   input: {
     height: 45,
-    width: '90%',
+    width: "90%",
     margin: 12,
     borderWidth: 1,
     borderColor: "gray",
