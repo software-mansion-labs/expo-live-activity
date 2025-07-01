f84c5d79327d7268c35bf715b47623628953650b
Anna Olak <15946812+anna1901@users.noreply.github.com>
2025-07-01 16:46:17 +0200

    Apply changes from CR

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import ExpoLiveActivityModule from './ExpoLiveActivityModule';
-import { Platform } from 'react-native';
+import ExpoLiveActivityModule from "./ExpoLiveActivityModule";
+import { Platform } from "react-native";
 
 export type LiveActivityState = {
-  title: string,
-  subtitle: string,
-  date: number
+  title: string;
+  subtitle: string;
+  date: number;
 };
 
 /**
  * @param {LiveActivityState} state The state for the live activity.
  * @returns {string} The identifier of the started activity.
- * @throws {Error} when function is called on platform different than iOS.
+ * @throws {Error} When function is called on platform different than iOS.
  */
 export function startActivity(state: LiveActivityState): string {
-  if (Platform.OS !== 'ios') {
-    throw new Error('startActivity is only available on iOS');
+  if (Platform.OS !== "ios") {
+    throw new Error("startActivity is only available on iOS");
   }
   return ExpoLiveActivityModule.startActivity(state);
 }
@@ -23,11 +23,11 @@ export function startActivity(state: LiveActivityState): string {
  * @param {string} id The identifier of the activity to stop.
  * @param {LiveActivityState} state The updated state for the live activity.
  * @returns {string} The identifier of the stopped activity.
- * @throws {Error} If not on iOS.
+ * @throws {Error} When function is called on platform different than iOS.
  */
 export function stopActivity(id: string, state: LiveActivityState): string {
-  if (Platform.OS !== 'ios') {
-    throw new Error('stopActivity is only available on iOS');
+  if (Platform.OS !== "ios") {
+    throw new Error("stopActivity is only available on iOS");
   }
   return ExpoLiveActivityModule.stopActivity(id, state);
 }
@@ -36,11 +36,11 @@ export function stopActivity(id: string, state: LiveActivityState): string {
  * @param {string} id The identifier of the activity to update.
  * @param {LiveActivityState} state The updated state for the live activity.
  * @returns {string} The identifier of the updated activity.
- * @throws {Error} If not on iOS.
+ * @throws {Error} When function is called on platform different than iOS.
  */
 export function updateActivity(id: string, state: LiveActivityState): string {
-  if (Platform.OS !== 'ios') {
-    throw new Error('updateActivity is only available on iOS');
+  if (Platform.OS !== "ios") {
+    throw new Error("updateActivity is only available on iOS");
   }
   return ExpoLiveActivityModule.updateActivity(id, state);
 }
