454595735d8064dda27a720197f63ac72b6291f6
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2025-09-29 18:19:25 +0200

    Fix `withPlist` (#68)

--- plugin/src/withPlist.ts
+++ plugin/src/withPlist.ts
@@ -1,26 +1,9 @@
-import { ConfigPlugin, InfoPlist, withInfoPlist } from '@expo/config-plugins'
-import plist from '@expo/plist'
-import { readFileSync, writeFileSync } from 'fs'
-import { join as joinPath } from 'path'
+import { ConfigPlugin, withInfoPlist } from '@expo/config-plugins'
 
-const withPlist: ConfigPlugin<{ targetName: string }> = (expoConfig, { targetName }) =>
+const withPlist: ConfigPlugin = (expoConfig) =>
   withInfoPlist(expoConfig, (plistConfig) => {
     const scheme = typeof expoConfig.scheme === 'string' ? expoConfig.scheme : expoConfig.ios?.bundleIdentifier
-
-    if (scheme) {
-      const targetPath = joinPath(plistConfig.modRequest.platformProjectRoot, targetName)
-      const filePath = joinPath(targetPath, 'Info.plist')
-      const content = plist.parse(readFileSync(filePath, 'utf8')) as InfoPlist
-
-      content.CFBundleURLTypes = [
-        {
-          CFBundleURLSchemes: [scheme],
-        },
-      ]
-
-      writeFileSync(filePath, plist.build(content))
-    }
-
+    if (scheme) plistConfig.modResults.CFBundleURLTypes = [{ CFBundleURLSchemes: [scheme] }]
     return plistConfig
   })
 
