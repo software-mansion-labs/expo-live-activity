cba0bdcddd4f8f7a0f1aecc1c9a051609b32ddb2
maciej.biegan <maciej.biegan@swmansion.com>
2025-11-10 13:58:03 +0100

    chore: format with prettier

--- plugin/src/withPlist.ts
+++ plugin/src/withPlist.ts
@@ -3,21 +3,16 @@ import { ConfigPlugin, withInfoPlist } from '@expo/config-plugins'
 const withPlist: ConfigPlugin = (expoConfig) =>
   withInfoPlist(expoConfig, (plistConfig) => {
     const scheme = typeof expoConfig.scheme === 'string' ? expoConfig.scheme : expoConfig.ios?.bundleIdentifier
-    
+
     if (scheme) {
       const existingURLTypes = plistConfig.modResults.CFBundleURLTypes || []
-      const schemeExists = existingURLTypes.some((urlType: any) => 
-        urlType.CFBundleURLSchemes?.includes(scheme)
-      )
-      
+      const schemeExists = existingURLTypes.some((urlType: any) => urlType.CFBundleURLSchemes?.includes(scheme))
+
       if (!schemeExists) {
-        plistConfig.modResults.CFBundleURLTypes = [
-          ...existingURLTypes,
-          { CFBundleURLSchemes: [scheme] }
-        ]
+        plistConfig.modResults.CFBundleURLTypes = [...existingURLTypes, { CFBundleURLSchemes: [scheme] }]
       }
     }
-    
+
     return plistConfig
   })
 
